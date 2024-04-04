068cc18261f2e817fced6a4be3a0599e33e760e7
Haohanblue <1147378768@qq.com>
2024-04-04 15:32:51 +0800

    feat: vue3ViteTS

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,30 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
 <template>
-    <div id="app">
-        <div class="header">
-            <tabBar></tabBar>
-        </div>
-        <router-view class="main"></router-view>
-        <div>
-            <Footer></Footer>
-        </div>
-    </div>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style>
-.app{
-    height: 100%;
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
 }
-.header{
-    height: 5%;
-    display: flex;
-    justify-content: left;
-    align-items: center;
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
 }
-.footer{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: #f5f5f5;
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
-
-<script setup>
-import TabBar from "@/components/tabBar.vue";
-import Footer from "@/components/footer.vue";
-</script>
